b114f78e85f56a06a8308ad3cc1579f4752a3b66
Kelly Coldren <shrtpvteacher@protonmail.com>
2024-03-29 15:38:11 -0700

    Removed ethers from Mint.js as it was not used

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
-import { ethers } from 'ethers';
+
 
 
 
